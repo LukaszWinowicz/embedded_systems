04382e95597157d5312e042de895b85a86e5fe56
Łukasz Winowicz <lwinowicz@gmail.com>
2023-11-13 18:01:27 +0100

    update current code

--- check.c
+++ check.c
@@ -15,43 +15,6 @@ int minutes_a = -1;
 int hours_a = -1;
 pthread_mutex_t lock;
 
-void *alarm_thread(void *arg)
-{
-    while (1) {
-        sleep(1);
-
-        pthread_mutex_lock(&lock);
-        if (done) {
-            pthread_mutex_unlock(&lock);
-            break;
-        }
-        pthread_mutex_unlock(&lock);
-
-        struct tm tm;
-        time_t t = time(NULL);
-        localtime_r(&t, &tm);
-
-        pthread_mutex_lock(&lock);
-
-        for (int i = 0; i < 5; i++)
-        {
-            sscanf(reminders[i], "%2d", &hours_a);
-            sscanf(reminders[i], "%*3c%2d", &minutes_a);
-            if (minutes_a == tm.tm_min && hours_a == tm.tm_hour) {
-                printf("<<ALARM>>\n");
-                printf("Hours: %d, Minutes: %d\n", hours_a, minutes_a);
-                minutes_a = -1;
-                hours_a = -1;
-                memset(reminders[i], 0, sizeof(reminders[i]));
-            }           
-        }
-
-        pthread_mutex_unlock(&lock);
-    }
-
-    return NULL;
-}
-
 void addReminder(const char *text, int hours, int minutes, int *counter) {
     if (*counter < 5) {
         for (int i = 0; i < 5; i++) {
@@ -72,7 +35,7 @@ void addReminder(const char *text, int hours, int minutes, int *counter) {
 void printReminders() {
     for (int i = 0; i < 5; i++) {
         if (reminders[i][0] != '\0') {
-            printf("[%d] - %s\n", i, reminders[i]);
+            printf("%s\n", reminders[i]);
         }
     }
 }
@@ -95,24 +58,31 @@ void parseInput(char *line, int *counter) {
         return;
     }
 
+    if (strcmp("check\n", line) == 0) {
+
+        for (int i = 0; i < 5; i++)
+        {
+            sscanf(reminders[i], "%2d", &hours_a);
+            sscanf(reminders[i], "%*3c%2d", &minutes_a);
+         
+            printf("Hours: %d, Minutes: %d\n", hours_a, minutes_a);
+        }
+
+    
+        return;
+    }
+
     if (sscanf(line, "%2d:%2d %[^\n]", &hours, &minutes, text) == 3 && hours >= 0 && hours <= 23 && minutes >= 0 && minutes <= 59) {
         addReminder(text, hours, minutes, counter);
     } else {
         printf("Invalid format or time values.\n");
     }
+
+    
 }
 
 int main(void) {
-pthread_t thread;
 
-    int ret = pthread_mutex_init(&lock, NULL);
-    if (ret)
-        return ret;
-    
-    ret = pthread_create(&thread, NULL, alarm_thread, NULL);
-    if (ret)
-        goto err_destroy_mutex;
-// -------------------------------------------
     int counter = 0;
     printf("Enter the data in the format 'HH:MM reminder message': \n");
 
@@ -130,17 +100,6 @@ pthread_t thread;
             clearInputBuffer();
         }
     }
-//---------------------------------------------
-     pthread_mutex_lock(&lock);
-    done = 1;
-    pthread_mutex_unlock(&lock);
 
-    ret = pthread_join(thread, NULL);
-    if (ret)
-        fprintf(stderr, "pthread_join() error");
-
-err_destroy_mutex:
-    pthread_mutex_destroy(&lock);
-
-    return ret;
+    return 0;
 }
