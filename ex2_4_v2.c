32e3c690e29597563d42d7cda4cbb4f917687d33
Łukasz Winowicz <lwinowicz@gmail.com>
2023-11-16 20:01:09 +0100

    added protect that user cant add reminder with whitespaces

--- ex2_4_v2.c
+++ ex2_4_v2.c
@@ -4,19 +4,23 @@
 #include <time.h>
 #include <string.h>
 #include <stdlib.h>
+#include <ctype.h>
 
 #define MAX_LINE_LENGTH 64
 
 char reminders[5][MAX_LINE_LENGTH] = {};
 int minutes = -1;
 int hours = -1;
+int minutes_alarm = -1;
+int hours_alarm = -1;
 int counter = 0;
 pthread_mutex_t lock;
 int text_lenght = MAX_LINE_LENGTH-7;
 
 void *alarm_thread(void *arg)
 {
-    while (1) {
+    while (1) 
+    {
         sleep(1);
         struct tm tm;
         time_t t = time(NULL);
@@ -25,14 +29,14 @@ void *alarm_thread(void *arg)
         pthread_mutex_lock(&lock);
             for (int i = 0; i < 5; i++)
             {
-                sscanf(reminders[i], "%2d", &hours);
-                sscanf(reminders[i], "%*3c%2d", &minutes);
-                if (minutes == tm.tm_min && hours == tm.tm_hour) 
+                sscanf(reminders[i], "%2d", &hours_alarm);
+                sscanf(reminders[i], "%*3c%2d", &minutes_alarm);
+                if (minutes_alarm == tm.tm_min && hours_alarm == tm.tm_hour) 
                 {
                     printf("\n| %s\n", reminders[i]);
                     memset(reminders[i], 0, sizeof(reminders[i]));
-                    minutes = -1;
-                    hours = -1;
+                    minutes_alarm = -1;
+                    hours_alarm = -1;                    
                     counter--;
                 }           
             }
@@ -46,7 +50,8 @@ void addReminder(const char *text, int hours, int minutes, int *counter)
 {
     if (*counter < 5) 
     {
-        for (int i = 0; i < 5; i++) {
+        for (int i = 0; i < 5; i++) 
+        {
             if (reminders[i][0] == '\0' || reminders[i][0] == '\n') 
             {
                 snprintf(reminders[i], sizeof(reminders[i]), "%02d:%02d %.*s", hours, minutes, text_lenght, text);
@@ -94,7 +99,21 @@ void parseInput(char *line, int *counter)
 
     if (sscanf(line, "%2d:%2d%*1[ ]%[^\n]", &hours, &minutes, text) == 3 && hours >= 0 && hours <= 23 && minutes >= 0 && minutes <= 59) 
     {
-        addReminder(text, hours, minutes, counter);
+        int onlySpaces = 1;
+        for (int i = 0; text[i] != '\0'; i++) {
+            if (!isspace((unsigned char)text[i])) {
+                onlySpaces = 0;
+                break;
+            }
+        }
+
+        if (onlySpaces) {
+            printf("The text consists of whitespace only.\n");
+        } else {
+            addReminder(text, hours, minutes, counter);
+        }
+
+        
     } 
     else 
     {
@@ -119,14 +138,16 @@ int main(void)
     while (1) {
         char line[MAX_LINE_LENGTH] = {};
         printf("> ");
-        if (fgets(line, sizeof(line), stdin) == NULL) {
+        if (fgets(line, sizeof(line), stdin) == NULL) 
+        {
             printf("Data reading error.\n");
             return 1;
         }
 
         parseInput(line, &counter);
 
-        if (strchr(line, '\n') == NULL) {
+        if (strchr(line, '\n') == NULL) 
+        {
             clearInputBuffer();
         }
 
@@ -142,3 +163,10 @@ int main(void)
 
     return ret;
 }
+
+
+/*
+1. Zablokować możliwość dodawania pustego wpisu
+2. Znak zachęty po pokazaniu się danych
+
+*/
