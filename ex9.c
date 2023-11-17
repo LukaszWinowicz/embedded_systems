a10c5becdb80105f540697aa80744887e83b732c
Łukasz Winowicz <lwinowicz@gmail.com>
2023-11-17 18:05:50 +0100

    added hangeman

--- ex9.c
+++ ex9.c
@@ -2,56 +2,57 @@
 #include <string.h>
 #include <ctype.h>
 
-void draw_hangman(int mistakes) {
+void draw_hangman(int mistakes) 
+{
     switch (mistakes) {
         case 0:
-            printf(stderr, " .----.\n");
-            printf(stderr, "       |\n");
-            printf(stderr, "       |\n");
-            printf(stderr, "       |\n");
-            printf(stderr, "      / \\\n");
+            printf(" .----.\n");
+            printf("       |\n");
+            printf("       |\n");
+            printf("       |\n");
+            printf("      / \\\n");
             break;
         case 1:
-            printf(stderr, " .----.\n");
-            printf(stderr, " O     |\n");
-            printf(stderr, "       |\n");
-            printf(stderr, "       |\n");
-            printf(stderr, "      / \\\n");
+            printf(" .----.\n");
+            printf(" O     |\n");
+            printf("       |\n");
+            printf("       |\n");
+            printf("      / \\\n");
             break;
         case 2:
-            printf(stderr, " .----.\n");
-            printf(stderr, " O     |\n");
-            printf(stderr, "  \    |\n");
-            printf(stderr, "       |\n");
-            printf(stderr, "      / \\\n");
+            printf(" .----.\n");
+            printf(" O     |\n");
+            printf("  \\    |\n");
+            printf("       |\n");
+            printf("      / \\\n");
             break;
         case 3:
-            printf(stderr, " .----.\n");
-            printf(stderr, " O     |\n");
-            printf(stderr, " |\    |\n");
-            printf(stderr, "       |\n");
-            printf(stderr, "      / \\\n");
+            printf(" .----.\n");
+            printf(" O     |\n");
+            printf(" |\\    |\n");
+            printf("       |\n");
+            printf("      / \\\n");
             break;
         case 4:
-            printf(stderr, " .----.\n");
-            printf(stderr, " O     |\n");
-            printf(stderr, "/|\    |\n");
-            printf(stderr, "       |\n");
-            printf(stderr, "      / \\\n");
+            printf(" .----.\n");
+            printf(" O     |\n");
+            printf("/|\\    |\n");
+            printf("       |\n");
+            printf("      / \\\n");
             break;
         case 5:
-            printf(stderr, " .----.\n");
-            printf(stderr, " O     |\n");
-            printf(stderr, "/|\    |\n");
-            printf(stderr, "/      |\n");
-            printf(stderr, "      / \\\n");
+            printf(" .----.\n");
+            printf(" O     |\n");
+            printf("/|\\    |\n");
+            printf("/      |\n");
+            printf("      / \\\n");
             break;
         case 6:
-            printf(stderr, " .----.\n");
-            printf(stderr, " O     |\n");
-            printf(stderr, "/|\    |\n");
-            printf(stderr, "/ \     |\n");
-            printf(stderr, "      / \\\n");
+            printf(" .----.\n");
+            printf(" O     |\n");
+            printf("/|\\    |\n");
+            printf("/ \\    |\n");
+            printf("      / \\\n");
             break;       
         default:
             printf("Niepoprawna liczba błędów.\n");
@@ -59,56 +60,72 @@ void draw_hangman(int mistakes) {
     }
 }
 
-int main() {
+int main() 
+{
     char secretWord[] = "Warszawa";
     char guessedLetters[strlen(secretWord)];
     memset(guessedLetters, '_', strlen(secretWord));
 
     char guess[100];
-    int attempts = 10; // Możesz zmienić liczbę prób
+    int mistakes = 0; // Licznik błędów
 
-    while (attempts > 0) {
-        printf("Aktualny stan słowa: %s\n", guessedLetters);
-        printf("Podaj literę lub próbuj odgadnąć słowo: ");
+    while (mistakes < 6) 
+    {
+        draw_hangman(mistakes); // Wyświetl stan wisielca
+        printf("----------------------------------------------------\n");
+        printf("Guess the word: %s\n", guessedLetters);
+        printf("Enter a letter or try to guess the word: ");
         fgets(guess, 100, stdin);
 
         // Usunięcie znaku nowej linii
         guess[strcspn(guess, "\n")] = 0;
 
-        if (strlen(guess) == 1) { // Gdy podana jest pojedyncza litera
+        if (strlen(guess) == 1) 
+        {
             int found = 0;
-            for (int i = 0; i < strlen(secretWord); i++) {
-                if (tolower(secretWord[i]) == tolower(guess[0])) {
+            for (int i = 0; i < strlen(secretWord); i++) 
+            {
+                if (tolower(secretWord[i]) == tolower(guess[0])) 
+                {
                     guessedLetters[i] = secretWord[i];
                     found = 1;
                 }
             }
 
-            if (!found) {
-                printf("Nie ma takiej litery!\n");
-                attempts--;
+            if (!found) 
+            {
+                printf("There is no such letter!\n");
+                mistakes++; // Zwiększ licznik błędów
             }
-        } else if (strlen(guess) == strlen(secretWord)) { // Gdy próba odgadnięcia całego słowa
-            if (strcasecmp(secretWord, guess) == 0) {
-                printf("Gratulacje, odgadłeś słowo: %s\n", secretWord);
+        } 
+        else if (strlen(guess) == strlen(secretWord)) 
+        {
+            if (strcasecmp(secretWord, guess) == 0) 
+            {
+                printf("Congratulations, you guessed the word: %s\n", secretWord);
                 return 0;
-            } else {
-                printf("To nie jest poprawne słowo!\n");
-                attempts--;
+            } 
+            else 
+            {
+                printf("This is not the correct word!\n");
+                mistakes++; // Zwiększ licznik błędów
             }
-        } else {
+        } 
+        else 
+        {
             printf("Niepoprawne wejście!\n");
-            attempts--;
+            mistakes++; // Zwiększ licznik błędów
         }
 
-        if (strcmp(guessedLetters, secretWord) == 0) {
-            printf("Gratulacje, odgadłeś słowo: %s\n", secretWord);
+        if (strcmp(guessedLetters, secretWord) == 0) 
+        {
+            printf("Congratulations, you guessed the word: %s\n", secretWord);
             return 0;
         }
 
-        printf("Pozostało prób: %d\n\n", attempts);
+        printf("There are attempts left: %d\n\n", 6 - mistakes);
     }
 
-    printf("Koniec gry! Słowo to: %s\n", secretWord);
+    printf("End of the game! This word: %s\n", secretWord);
     return 0;
 }
