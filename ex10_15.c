a84e2c659fa25139aa0c1ecb0c5e706f67858abb
Łukasz Winowicz <lwinowicz@gmail.com>
2023-11-18 17:05:09 +0100

    ex14 completed

--- ex10_15.c
+++ ex10_15.c
@@ -1,4 +1,7 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
 
 #define MAP_W 5
 #define MAP_H 5
@@ -7,7 +10,7 @@ struct character {
     char c;
     int x;
     int y;
-}
+};
 
 /* Jeżeli dir to 'w', 's', 'a' lub 'd' to rusz graczem o jedną jednostkę w odpowiednim kierunku.
  * Pamiętaj że gracz nie może wyjść poza granice planszy (5x5) */
@@ -18,25 +21,51 @@ void character_move(struct character *ch, char dir){
     {
         // warunek ? wartość_gdy_prawda : wartość_gdy_fałsz
         ch->y = ch->y > 0 ? ch->y-1 : ch->y;
-    }
+    };
 
     if (dir == 's') // dół
     {
         ch->y = ch->y < 4 ? ch->y+1 : ch->y;
-    }
+    };
     
     if (dir == 'a') // lewo
     {
         ch->x = ch->x > 0 ? ch->x-1 : ch->x;
-    }
+    };
 
     if (dir == 'd') // prawo
     {
-        ch->x = ch->x < 4 ? ch->x-1 : ch->x;
-    }
-
+        ch->x = ch->x < 4 ? ch->x+1 : ch->x;
+    };
     
 };
+
+void character_random_move(struct character *c)
+{
+    int x = rand()%5;
+
+    if (x == 0)
+    {
+        character_move(c, 'w');
+    };
+
+    if (x == 1)
+    {
+        character_move(c, 's');
+    };
+
+    if (x == 2)
+    {
+        character_move(c, 'a');
+    };
+
+    if (x == 3)
+    {
+        character_move(c, 'd');
+    };
+    
+
+}
 
 struct frame {
     char cells[MAP_W][MAP_H];
@@ -44,13 +73,13 @@ struct frame {
 
 /* wyczyść bufor znakami '.' */
 void frame_clear(struct frame *frame){
-    for (int) i = 0; i < MAP_W; i++)
+    for (int i = 0; i < MAP_W; i++)
     {
         for (int j = 0; j < MAP_H; j++)
         {
             frame->cells[i][j] = '.';
-        }        
-    }    
+        };
+    };
 };
 
 /* umieść gracza buforze */
@@ -60,43 +89,46 @@ void frame_add(struct frame *frame, struct character *ch){
  
 /* wypisz bufor */
 void frame_draw(struct frame *frame){
-    for (int) i = 0; i < MAP_W; i++)
+    for (int i = 0; i < MAP_W; i++)
     {
         for (int j = 0; j < MAP_H; j++)
         {
             printf("%c", frame->cells[i][j]);
         }
         printf("\n");
-    }    
+    };
 };
 
 int main(void){
-struct character player = {'&', 0, 0};
-struct character enemies[] = {
-    {'A', 0, MAP_H-1},
-    {'B', MAP_W-1, 0},
-    {"C", 2, 2}
-};
+    struct character player = {'&', 0, 0};
+    struct character enemies[] = {
+        {'A', 0, MAP_H-1},
+        {'B', MAP_W-1, 0},
+        {'C', MAP_W-1, MAP_H-1}
+    };
 
-while (1)
-{
-    struct frame frame;
-    frame_clear(&frame);
-    for (int i = 0; i < sizeof(enemies)/sizeof(enemies[0]); i++)
+    while (1)
     {
-        /* code */
+        struct frame frame;
+        frame_clear(&frame);
+        for (int i = 0; i < sizeof(enemies)/sizeof(enemies[0]); i++)
+        {
+             frame_add(&frame, &enemies[i]);
+        };
+
+        frame_add(&frame, &player);
+        frame_draw(&frame);
+
+        char c;
+        if (scanf(" %c", &c) == 1)
+        {
+            character_move(&player, c);
+        };
+        
+        for (int i = 0; i < sizeof(enemies)/sizeof(enemies[0]); i++)
+        {
+            character_random_move(&enemies[i]);
+        }
+
     }
-    frame_add(&frame, &player);
-    frame_draw(&frame);
-
-    char c;
-    if (scanf(" %c", &c) == 1)
-    {
-        character_move(&player, c);
-    }
-    
-    
-}
-
-
 }
