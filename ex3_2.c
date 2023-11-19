0e48d48876e7bbc472612a5509aea44672117a7d
Łukasz Winowicz <lwinowicz@gmail.com>
2023-11-19 13:18:40 +0100

    save

---
+++ ex3_2.c
@@ -0,0 +1,5 @@
+/*
+Wyświetlanie plików
+Celem zadania jest implementacja własnej uproszczonej wersji programu cat. 
+Program powinien umożliwiać wypisanie pliku podanego jako pierwszy argument na standardowe wyjście.
+*/
