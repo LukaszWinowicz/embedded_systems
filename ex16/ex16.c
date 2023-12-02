a42176a137e8a35eff482a8f41ec9cc632c942cc
Łukasz Winowicz <lwinowicz@gmail.com>
2023-12-02 13:05:47 +0100

    create working ex16

--- ex16/ex16.c
+++ ex16/ex16.c
@@ -1,17 +1,53 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
 #include "hangman.h"
 #include "rps.h"
 #include "movement.h"
 
-int main(void){
-    int c;
-    do
-    {
-        printf("> ");
-        scanf("%d", &c);
+#define NUM_GAMES 4
+char* games[NUM_GAMES] = { "hangman", "rps", "movement", "exit"};
 
-        switch (c)
-        {
+void displayMenu() 
+{
+    printf("          _           \n");                                      
+    printf("         | |          \n");                                      
+    printf(" _ __ ___| |_ _ __ ___    __ _  __ _ _ __ ___   ___  ___ \n");
+    printf("| '__/ _ \\ __| '__/ _ \\  / _` |/ _` | '_ ` _ \\ / _ \\/ __|\n");
+    printf("| | |  __/ |_| | | (_) || (_| | (_| | | | | | |  __/\\__ \\\n");
+    printf("|_|  \\___|\\__|_|  \\___/  \\__, |\\__,_|_| |_| |_|\\___||___/\n");
+    printf("                         __/ |                          \n"); 
+    printf("                        |___/                           \n");
+
+    for (int i = 1; i <= NUM_GAMES; ++i)
+    {
+        printf("%d) %s\n", i, games[i - 1]);        
+    }
+}
+
+
+void clearInputBuffer() {
+    int c;
+    while ((c = getchar()) != '\n' && c != EOF) { }
+}
+
+int main(void) {
+    while (1) {
+        system("clear"); 
+        displayMenu();
+        printf("> ");
+
+        int d;
+        if (1 != scanf("%d", &d)) {
+            clearInputBuffer();  // Czyści bufor, jeśli scanf nie odczyta liczby
+            continue;
+        }
+
+        clearInputBuffer();  // Czyści bufor po poprawnym odczytaniu liczby
+        system("clear");
+        switch (d) {
+           // system("clear");
         case 1:
             hangman();
             break;
@@ -19,13 +55,15 @@ int main(void){
             rps();
             break;
         case 3:
-            movement();
+            movement();            
             break;
-
+        case 4:
+            return 0;
         default:
-            break;
+            printf("Invalid choice. Please try again.\n");
         }
+        sleep(1);
+    }
 
-    } while (c != 4);
-    
+    return 0;
 }
