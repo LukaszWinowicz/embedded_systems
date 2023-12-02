08e5fac6a47d76c5ce2f2994c90f61b495204dce
Łukasz Winowicz <lwinowicz@gmail.com>
2023-12-02 12:04:51 +0100

    create menu

---
+++ ex16/ex16.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main(void){
+    int c;
+    do
+    {
+        printf("> ");
+        scanf("%d", &c);
+
+        switch (c)
+        {
+        case 1:
+            hangman();
+            break;
+        case 2:
+            rps();
+            break;
+        case 3:
+            movement();
+            break;
+            
+        default:
+            break;
+        }
+
+    } while (c != 4);
+    
+}
