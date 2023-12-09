f7e5fe50ec7faa306a0826d23ecd2bbf3bb801d1
Łukasz Winowicz <lwinowicz@gmail.com>
2023-12-09 16:36:45 +0100

    create working script with buuble sorting

--- dzpaisd/ex1.cpp
+++ dzpaisd/ex1.cpp
@@ -7,11 +7,31 @@ Złożoność tego sposobu sortowania jest rzędu O(n^2).
 using namespace std;
 
 void bubble_sort(int tab[], int n){
+    bool isSorted = false;
+    while(!isSorted)
+    {
+        isSorted = true;
+        for (int j= 0; j < n -1; j++)
+        {
+            if (tab[j] > tab[j+1])
+            {
+                int tmp = tab[j];
+                tab[j] = tab[j+1];
+                tab[j+1] = tmp;
+                isSorted = false;
+            }
+        }
+    }
 
+    for (int i= 0; i < n; i++)
+    {
+        cout << tab[i] << " ";
+    }
+    cout << endl;
 }
 
-int main(){
-
-
-    return 0;
+int main()
+{
+    int tab[] = {1,7,3,0,2};
+    bubble_sort(tab, 5);
 }
