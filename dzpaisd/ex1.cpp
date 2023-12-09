797d0a3ad52bf7b3f718dfb413b14d25d2cabfef
Łukasz Winowicz <lwinowicz@gmail.com>
2023-12-09 16:19:43 +0100

    create new ex1 with buuble sorting

---
+++ dzpaisd/ex1.cpp
@@ -0,0 +1,17 @@
+/* Zadanie 1 - sortowanie bąbelkowe jest jednym z najprostszych w implementacji algorytmów porządkujących dane. 
+Złożoność tego sposobu sortowania jest rzędu O(n^2).
+
+
+*/
+#include <iostream>
+using namespace std;
+
+void bubble_sort(int tab[], int n){
+
+}
+
+int main(){
+
+
+    return 0;
+}
