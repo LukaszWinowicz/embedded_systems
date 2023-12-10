a49643ce0066eda91a841d3ed2a89c401d0cc6e6
Łukasz Winowicz <lwinowicz@gmail.com>
2023-12-10 08:40:29 +0100

    update ex4 story

--- dzpaisd/ex4.cpp
+++ dzpaisd/ex4.cpp
@@ -1,17 +1,21 @@
-/*Stwórz klasę "wektor, która będzie posiadać pole int* table
-W konstruktorze tabela powinna być zaalakowana z rozmiarem 10 elementów
-W destruktorze tablica powinna być zwolniona*/
+/*
+Stwórz klasę "wektor, która będzie posiadać pole int* table.
+W konstruktorze tabela powinna być zaalakowana z rozmiarem 10 elementów.
+W destruktorze tablica powinna być zwolniona.
+Stwrz metodę push_back(int val), która umieszcza elementy na pierwszym wolnym miejscu w tabeli.
+Jeżeli dodamy element na ostatnim miejscu tabeli, powinna ona zostać rozszerzona o następne 10 elementów.
+*/
 
 #include <iostream>
 using namespace std;
 
 class myVector{
     public:
-    int* tab ;
+    int* tab;
 
     // Konstruktor:
     myVector(){
-        new int [10];
+        new int[10];
 
     }
 
@@ -19,6 +23,7 @@ class myVector{
     ~myVector(){
         delete tab;
     }
+
 };
 
 int main(){
