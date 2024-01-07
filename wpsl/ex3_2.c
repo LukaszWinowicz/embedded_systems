ba087aa766697b7598cbe6da8730aba92a5817db
Łukasz Winowicz <lwinowicz@gmail.com>
2024-01-07 12:39:24 +0100

    remove old code from wpsl folder, create new 4 folders in wpsl with 4 different topics

--- wpsl/ex3_2.c
+++
@@ -1,5 +0,0 @@
-/*
-Wyświetlanie plików
-Celem zadania jest implementacja własnej uproszczonej wersji programu cat. 
-Program powinien umożliwiać wypisanie pliku podanego jako pierwszy argument na standardowe wyjście.
-*/
