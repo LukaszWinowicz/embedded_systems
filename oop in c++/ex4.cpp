b643606ee543567d997261d0dcce69de40c66e71
Łukasz Winowicz <lwinowicz@gmail.com>
2024-01-19 21:00:27 +0100

    finish ex4

--- oop in c++/ex4.cpp
+++ oop in c++/ex4.cpp
@@ -11,6 +11,8 @@ Reverse a table:
 std::vector<>
 */
 #include <iostream>
+#include <vector>
+#include <algorithm>  // dla reverse
 using namespace std;
 
 int main(){
@@ -19,6 +21,31 @@ int main(){
     int cStyleArray[] = {1, 2, 3};
     int arraySize = sizeof(cStyleArray) / sizeof(cStyleArray[0]);
 
-    cout << cStyleArray << endl;
-    cout << arraySize << endl;
+    // Vector (kontener - dynamiczna tablica)
+    vector<int> Array = {4, 5, 6};
+
+    // Nowa tablica łącząca cStyleArray + Array
+    vector<int> newArray;
+    newArray.reserve(arraySize + Array.size());
+
+    // Dodanie elementów z tablicy w stylu C
+    for (int i = 0; i < arraySize; ++i) {
+        newArray.push_back(cStyleArray[i]);
+    }
+
+    // Dodaj elementy z vector
+    newArray.insert(newArray.end(), Array.begin(), Array.end());
+
+    // Odwrócenie kolejności elementów
+    reverse(newArray.begin(), newArray.end());
+
+    // Wyświetl wynik
+    for (int val : newArray) {
+        cout << val << " ";
+    }
+
+    cout << endl;
+
+    return 0;
+
 }
