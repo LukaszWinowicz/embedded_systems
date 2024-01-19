10166c973f52f5740be30f6070e37089241d9872
Łukasz Winowicz <lwinowicz@gmail.com>
2024-01-19 20:45:23 +0100

    ex4 - create array in c style

--- oop in c++/ex4.cpp
+++ oop in c++/ex4.cpp
@@ -10,3 +10,15 @@ Reverse a table:
 []
 std::vector<>
 */
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    // Tablica w stylu C
+    int cStyleArray[] = {1, 2, 3};
+    int arraySize = sizeof(cStyleArray) / sizeof(cStyleArray[0]);
+
+    cout << cStyleArray << endl;
+    cout << arraySize << endl;
+}
