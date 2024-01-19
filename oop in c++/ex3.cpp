323371127ed8b3c92e1c614a66be7c70c34d9cd7
Łukasz Winowicz <lwinowicz@gmail.com>
2024-01-19 19:50:43 +0100

    created reversed method (without polish letters)

--- oop in c++/ex3.cpp
+++ oop in c++/ex3.cpp
@@ -12,3 +12,50 @@ Reverse a string:
 char*
 std::string
 */
+
+#include <iostream>
+#include <string>
+#include <cstring>
+using namespace std;
+
+int reversedAsString(){
+    string word = "Warszawa";
+    string reversed = "";
+
+    cout << word << endl;
+
+    // Odwrócenie słowa ręcznie
+    for (int i = word.length() - 1; i >= 0; --i)
+    {
+       reversed += word[i];
+    }
+    
+    cout << reversed << endl;
+
+    return 0;
+
+}
+
+int reverseAsChar(){
+    char word[] = "Warszawa";
+    int length = strlen(word);
+    char reversed[length + 1]; // tutaj uwzględniam pusty znak po ostatniej literze
+    cout << word << endl;
+
+    for (int i = 0; i < length; i++)
+    {
+        reversed[i] = word[length - 1 - i];
+    }
+    reversed[length] = '\0'; // Dodanie znaku null na koniec
+
+    cout << reversed << endl;
+
+    return 0;
+}
+
+
+
+int main(){
+    reverseAsChar();
+    return 0;
+}
