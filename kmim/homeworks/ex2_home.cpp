083db4beabd567e72e88b67ce19d9c6aed51a8ea
Łukasz Winowicz <lwinowicz@gmail.com>
2024-01-21 13:48:36 +0100

    ex2 - added loop with find method that check there is in vector rand value

--- kmim/homeworks/ex2_home.cpp
+++ kmim/homeworks/ex2_home.cpp
@@ -20,9 +20,13 @@ Liczba elementów tablicy - n
 #include <vector>
 #include <cstdlib>
 #include <ctime>
-using namespace std;
+#include <mutex>
+#include <algorithm>
 
+using namespace std;
+mutex numbers_mutex;
 vector<int> numbers;
+int counter = 0;
 
 void message(int threadNum){
     
@@ -30,27 +34,21 @@ void message(int threadNum){
     srand(time(nullptr) + threadNum);
 
     // Losowanie liczby i wypisanie wiadomości   
-    bool isUnique = true;
 
-    while (isUnique == false)
+    while (true)
     {
-        int randomValue = (rand() % 5) + 1; // zakres od 1 do 5
-
-        for (int i = 0; i < numbers.size(); i++)
-        {
-            if(numbers[i] == randomValue)
-            {
-                isUnique = false;
-            }
-        }
-
-        if (isUnique == true)
-        {
+        int randomValue = (rand() % 10) + 1; // zakres od 1 do 5
+        cout << "Adres w pamięci(" << counter << "): " << &randomValue << "\n" << endl;
+        counter++;
+        lock_guard<mutex> guard(numbers_mutex); // Zapewnienie bezpieczeństwa wątków
+        
+        if (find(numbers.begin(), numbers.end(), randomValue) == numbers.end()) {
+            // Liczba jest unikalna
             numbers.push_back(randomValue);
             cout << "Wątek " << threadNum << " wylosował wartość: " << randomValue << endl;
+            break; // Wyjście z pętli
         }
-        
-    }
+    }    
 }
 
 int main(){
@@ -58,17 +56,22 @@ int main(){
     thread thr1(message, 1); // Przekazanie funkcji i argumentu do wątku
     thread thr2(message, 2);
     thread thr3(message, 3);
+    thread thr4(message, 4);
+    thread thr5(message, 5);
+    thread thr6(message, 6);
 
     thr1.join();
     thr2.join();
     thr3.join();
+    thr4.join();
+    thr5.join();
+    thr6.join();
     
     for (int i = 0; i < numbers.size(); i++)
     {
         cout << numbers[i] << " | ";
     }
     cout << endl;
-    
 
     return 0;
 }
