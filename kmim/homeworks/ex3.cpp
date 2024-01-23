17c3685be1b865c558a7983588c80b4028896ebf
Łukasz Winowicz <lwinowicz@gmail.com>
2024-01-23 19:42:53 +0100

    added mutex, created enum and contert do string

--- kmim/homeworks/ex3.cpp
+++ kmim/homeworks/ex3.cpp
@@ -1,27 +1,44 @@
 #include <iostream>
 #include <thread>
-
+#include <mutex>
 using namespace std;
-int WINNING_MIN = 10;
-int player1Score = 0;
-int player2Score = 0;
+mutex mtx;  // Mutex do synchronizacji wypisów na konsolę
 
-void player(int playerNumber){
+enum class Choose { Papier = 1, Kamien = 2, Nozyce = 3 };
+
+Choose losujWybor() {
+    return static_cast<Choose>(rand() % 3 + 1);
+}
+
+string wyborNaString(Choose wybor) {
+    switch (wybor) {
+        case Choose::Papier: return "Papier";
+        case Choose::Kamien: return "Kamien";
+        case Choose::Nozyce: return "Nozyce";
+        default: return "Nieznany";
+    }
+}
+
+void playerFunction(int playerNumber) {
+    
+    // Inicjalizacja generatora liczb losowych
+    unique_lock<std::mutex> lock(mtx);
+    srand(time(nullptr) + playerNumber);
+    Choose wyborGracza = losujWybor();
+    cout << "Gracz "<< playerNumber << " wybrał: " << wyborNaString(wyborGracza) << endl;
+
 
 }
 
+int main() {
+    thread player1(playerFunction, 1);
+    thread player2(playerFunction, 2);
 
-int main(){
-    thread player1(player, 1);
-    thread player2(player, 2);
-
-    while (player1Score == WINNING_MIN || player2Score == WINNING_MIN)
-    {
-        /* code */
-    }
     
 
+    // Oczekiwanie na zakończenie wątków
+    player1.join();
+    player2.join();
 
-    watekGracza1.join();
-    watekGracza2.join();
+    return 0;
 }
