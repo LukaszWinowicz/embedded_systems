43cdaa8d60a37735bb33fe7b15cd5748308f8f5d
Łukasz Winowicz <lwinowicz@gmail.com>
2024-01-25 11:05:16 +0100

    clear ex3, fresh start

--- kmim/homeworks/ex3.cpp
+++ kmim/homeworks/ex3.cpp
@@ -2,38 +2,35 @@
 std::string roc = "1) ✌️Rock\n";
   std::string pap = "2) ✋Paper\n";
   std::string sci = "3) ✌️Scissors\n";
+
+    Napisz program który emuluje grę kamień nożyce papier. 
+    Program tworzy 2 wątki, które losują sobie jeden z elementów. 
+    Przekazują go do arbitra (proces główny), który określa kto wygrał. 
+    Gra toczy się do 10 zwycięstw. Podczas pojedynczej tury gry obowiązuje zasada, że pierwszy wpisuje ten co przegrał.
+
 */
 
 #include <iostream>
 #include <thread>
 #include <mutex>
-#include <future>
-#include <cstdlib>
-#include <ctime>
 
 using namespace std;
-
-mutex mtx;  // Mutex do synchronizacji
-int WINNING_MIN = 10;
 int player1Score = 0;
 int player2Score = 0;
 
-int playerFunction(int playerNumber){
-    // Inicjalizacja generatora liczb losowych
-    srand(time(nullptr) + playerNumber);
-    lock_guard<mutex> lock(mtx);
-    int playerChoose = rand() % 3 + 1;
-    return playerChoose;
+mutex mtx;
+
+void player() {
+    
 }
 
 int main() {
-    // Uruchomienie wątków za pomocą std::async
-    thread player1Result(playerFunction, 1);
-    thread player2Result(playerFunction, 2);
+       
+    thread player1(player, 1);
+    thread player2(player, 2);
 
-    // Oczekiwanie na wyniki i ich wyświetlenie
-    cout << "Gracz 1 wybrał: " << "✌️" << endl;
-    cout << "Gracz 2 wybrał: " << "✋" << endl;
+    player1.join();
+    player2.join();
 
     return 0;
 }
--- kmim/homeworks/example.cpp
+++ kmim/homeworks/example.cpp
@@ -1,106 +1,75 @@
 #include <iostream>
-#include <cstdlib>
-#include <ctime>
-using namespace std;
+#include <thread>
+#include <mutex>
+#include <condition_variable>
+#include <random>
+#include <vector>
 
-int rule(char p, char c){
-    if (p == c){
-        return 0;
+enum class Wybor { Kamien, Papier, Nozyce };
+const int MAX_WYGRANYCH = 10;
+
+std::mutex mtx;
+std::condition_variable cv;
+Wybor wyborGracza1, wyborGracza2;
+bool turaZakonczona = true;
+int wygraneGracza1 = 0, wygraneGracza2 = 0;
+
+// Losowanie wyboru gracza
+Wybor losujWybor() {
+    static std::random_device rd;
+    static std::mt19937 gen(rd());
+    static std::uniform_int_distribution<> dis(0, 2);
+    return static_cast<Wybor>(dis(gen));
+}
+
+// Funkcja decydująca kto wygrał
+int decyduj(Wybor w1, Wybor w2) {
+    if (w1 == w2) return 0;
+    if ((w1 == Wybor::Kamien && w2 == Wybor::Nozyce) || 
+        (w1 == Wybor::Papier && w2 == Wybor::Kamien) ||
+        (w1 == Wybor::Nozyce && w2 == Wybor::Papier)) return 1;
+    return -1;
+}
+
+void gracz(int numerGracza) {
+    while (wygraneGracza1 < MAX_WYGRANYCH && wygraneGracza2 < MAX_WYGRANYCH) {
+        std::unique_lock<std::mutex> lock(mtx);
+        cv.wait(lock, []{ return turaZakonczona; });
+
+        if (numerGracza == 1) {
+            wyborGracza1 = losujWybor();
+            std::cout << "Gracz 1 wybrał." << std::endl;
+        } else {
+            wyborGracza2 = losujWybor();
+            std::cout << "Gracz 2 wybrał." << std::endl;
+        }
+        turaZakonczona = false;
+        cv.notify_all();
+
+        // Oczekiwanie na wynik
+        cv.wait(lock, []{ return turaZakonczona; });
+    }
+}
+
+int main() {
+    std::thread gracz1(gracz, 1);
+    std::thread gracz2(gracz, 2);
+
+    while (wygraneGracza1 < MAX_WYGRANYCH && wygraneGracza2 < MAX_WYGRANYCH) {
+        std::unique_lock<std::mutex> lock(mtx);
+        cv.wait(lock, []{ return !turaZakonczona; });
+
+        int wynik = decyduj(wyborGracza1, wyborGracza2);
+        if (wynik > 0) wygraneGracza1++;
+        else if (wynik < 0) wygraneGracza2++;
+
+        turaZakonczona = true;
+        cv.notify_all();
     }
 
-    if (p == 'r' && c == 'p'){
-        return -1;
-    }
-    else if (p == 's' && c == 'p'){
-        return 1;
-    }
-    else if (p == 'p' && c == 'r'){
-        return 1;
-    }
-    else if (p == 's' && c == 'r'){
-        return -1;
-    }
-    else if (p == 'r' && c == 's'){
-        return 1;
-    }
-    else if (p == 'p' && c == 's'){
-        return -1;
-    }
-};
+    gracz1.join();
+    gracz2.join();
 
-int main(){
-
-    char computer;
-    char player;
-    char playmore;
-    cout << "\t\t\t\t";
-    for(int i = 0; i < 50; i++){
-        cout << "-";
-    }
-    cout << endl;
-    cout << "\t\t\t\t";
-    cout << "\t Welcome to Rock, Paper and Scissors Game" << endl;
-    cout << "\t\t\t\t";
-    for(int i = 0; i < 50; i++){
-        cout << "-";
-    }
-    cout << endl;
-    cout << "\t\t\t\t";
-    cout << "\t Note: " << endl;
-    cout << "\t\t\t\t";
-    cout << "\t\t r : Rock" << endl << "\t\t\t\t" << "\t\t p - Paper" << endl << "\t\t\t\t" << "\t\t scissor" << endl << "\t\t\t\t"<< endl << endl;
-    cout << "\t\t\t\t";
-    for(int i = 0; i < 50; i++){
-        cout << "-";
-    }
-    cout << endl;
-    do{
-        int number = 0;
-        srand(time(0));        // initialized time to 0
-        number = rand() % 100; // will choose a number in range 0 - 99
-        // r - for rock
-        // p - for paper
-        // s - for scissors
-        if (number < 33)
-        {
-            computer = 'r';
-        }
-        else if (number > 66)
-        {
-            computer = 's';
-        }
-        else
-        {
-            computer = 'p';
-        }
-        // cout << "Note: \"r\" for \"Rock\", \"p\" for \"Paper\", \"s\" for \"Scissor\"." << endl;
-        cout << "\t\t\t\t";
-        cout << "Enter your choice: ";
-        cin >> player;
-        int result = rule(player, computer);
-        if(result == 1){
-            cout << "\t\t\t\t";
-            cout << "You won! Hurray" << endl;
-        }
-        else if(result == -1){
-            cout << "\t\t\t\t";
-            cout << "You lose! Bad Luck" << endl;
-        }
-        else{
-            cout << "\t\t\t\t";
-            cout << "Woah! That's Tie!" << endl;
-        }
-        cout << "\t\t\t\t";
-        cout << "Do you want to Play Again?" << endl;
-        cout << "\t\t\t\t";
-        cout << "Note: Press 'n' to exit! Press Anything to continue: ";
-        cin >> playmore;
-        cout << "\t\t\t\t";
-        for(int i = 0; i < 50; i++){
-            cout << "-";
-        }
-        cout << endl;
-    }while(playmore != 'n');
-
+    std::cout << "Koniec gry! Wyniki: Gracz 1 - " << wygraneGracza1 << ", Gracz 2 - " << wygraneGracza2 << std::endl;
     return 0;
 }
