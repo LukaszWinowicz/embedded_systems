2d11aa9a9693d7a606bceeee2c9834f7020a0402
Łukasz Winowicz <lwinowicz@gmail.com>
2024-01-25 10:51:12 +0100

    added emoji rock/paper/scisors

--- kmim/homeworks/ex3.cpp
+++ kmim/homeworks/ex3.cpp
@@ -1,45 +1,39 @@
+/*
+std::string roc = "1) ✌️Rock\n";
+  std::string pap = "2) ✋Paper\n";
+  std::string sci = "3) ✌️Scissors\n";
+*/
+
 #include <iostream>
 #include <thread>
 #include <mutex>
+#include <future>
+#include <cstdlib>
+#include <ctime>
+
 using namespace std;
 
-mutex mtx;  // Mutex do synchronizacji wypisów na konsolę
+mutex mtx;  // Mutex do synchronizacji
 int WINNING_MIN = 10;
 int player1Score = 0;
 int player2Score = 0;
 
 int playerFunction(int playerNumber){
-  // Inicjalizacja generatora liczb losowych
+    // Inicjalizacja generatora liczb losowych
     srand(time(nullptr) + playerNumber);
-
     lock_guard<mutex> lock(mtx);
     int playerChoose = rand() % 3 + 1;
-
     return playerChoose;
-  
 }
 
-
-
 int main() {
-  
+    // Uruchomienie wątków za pomocą std::async
+    thread player1Result(playerFunction, 1);
+    thread player2Result(playerFunction, 2);
 
-    //while (player1Score < WINNING_MIN && player2Score < WINNING_MIN)
-    //{
-        thread player1(playerFunction, 1);
-        thread player2(playerFunction, 2);
-
-        // 1 - papier, 2 kamień, 3 - nożyce
-        
-cout << "Gracz 1 " << player1.playerChoose() << endl;
-
-        // Oczekiwanie na zakończenie wątków
-        player1.join();
-        player2.join();
-   // }
-    
-
-    
+    // Oczekiwanie na wyniki i ich wyświetlenie
+    cout << "Gracz 1 wybrał: " << "✌️" << endl;
+    cout << "Gracz 2 wybrał: " << "✋" << endl;
 
     return 0;
 }
