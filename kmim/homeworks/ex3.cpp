85bd4f4e4190934091b85ee73edee45195f830a0
Łukasz Winowicz <lwinowicz@gmail.com>
2024-01-25 12:31:25 +0100

    created ex3, need to polish

--- kmim/homeworks/ex3.cpp
+++ kmim/homeworks/ex3.cpp
@@ -13,24 +13,66 @@ std::string roc = "1) ✌️Rock\n";
 #include <iostream>
 #include <thread>
 #include <mutex>
+#include <random>
 
 using namespace std;
+
 int player1Score = 0;
 int player2Score = 0;
-
+int player1Choose = 0;
+int player2Choose = 0;
+int startingPlayer = 1;
 mutex mtx;
 
-void player() {
-    
+void player(int playerNumber, int &playerChoice) {
+    unique_lock<mutex> lock(mtx);
+
+    // Tworzenie lokalnego generatora liczb losowych
+    random_device rd;
+    mt19937 gen(rd() + playerNumber);
+    uniform_int_distribution<> dis(1, 3);
+
+    playerChoice = dis(gen);    
+
+    cout << "Gracz " << playerNumber << " wybrał: " << playerChoice << endl;
 }
 
-int main() {
-       
-    thread player1(player, 1);
-    thread player2(player, 2);
 
-    player1.join();
-    player2.join();
+int main() {
+    
+
+    for (int i = 0; i < 10; i++) {
+
+        if (startingPlayer == 1) {
+            thread player1(player, 1, ref(player1Choose));
+            thread player2(player, 2, ref(player2Choose));
+            player1.join();
+        player2.join();
+        } 
+        else {
+            thread player2(player, 2, ref(player2Choose));        
+            thread player1(player, 1, ref(player1Choose));
+            player1.join();
+        player2.join();
+        }
+
+        if (player1Choose == player2Choose) {
+            cout << "REMIS" << endl;
+        } else if ((player1Choose == 1 && player2Choose == 3) ||
+                (player1Choose == 2 && player2Choose == 1) ||
+                (player1Choose == 3 && player2Choose == 2)) {
+            cout << "WYGRAŁ GRACZ 1" << endl;
+            player1Score++;
+        } else {
+            cout << "WYGRAŁ GRACZ 2" << endl;
+            player2Score++;
+        }
+
+        
+
+        
+    }
 
     return 0;
 }
+
--- kmim/homeworks/example.cpp
+++ kmim/homeworks/example.cpp
@@ -1,75 +1,53 @@
 #include <iostream>
 #include <thread>
-#include <mutex>
-#include <condition_variable>
-#include <random>
-#include <vector>
+#include <cstdlib>
+#include <ctime>
 
-enum class Wybor { Kamien, Papier, Nozyce };
 const int MAX_WYGRANYCH = 10;
 
-std::mutex mtx;
-std::condition_variable cv;
-Wybor wyborGracza1, wyborGracza2;
-bool turaZakonczona = true;
-int wygraneGracza1 = 0, wygraneGracza2 = 0;
-
-// Losowanie wyboru gracza
-Wybor losujWybor() {
-    static std::random_device rd;
-    static std::mt19937 gen(rd());
-    static std::uniform_int_distribution<> dis(0, 2);
-    return static_cast<Wybor>(dis(gen));
+int losujWybor(int playerNumber) {
+    srand(time(nullptr) + playerNumber);
+    return rand() % 3 + 1;  // 1 - kamień, 2 - papier, 3 - nożyce
 }
 
-// Funkcja decydująca kto wygrał
-int decyduj(Wybor w1, Wybor w2) {
-    if (w1 == w2) return 0;
-    if ((w1 == Wybor::Kamien && w2 == Wybor::Nozyce) || 
-        (w1 == Wybor::Papier && w2 == Wybor::Kamien) ||
-        (w1 == Wybor::Nozyce && w2 == Wybor::Papier)) return 1;
+int decyduj(int wybor1, int wybor2) {
+    if (wybor1 == wybor2) return 0;
+    if ((wybor1 == 1 && wybor2 == 3) || 
+        (wybor1 == 2 && wybor2 == 1) ||
+        (wybor1 == 3 && wybor2 == 2)) return 1;
     return -1;
 }
 
-void gracz(int numerGracza) {
-    while (wygraneGracza1 < MAX_WYGRANYCH && wygraneGracza2 < MAX_WYGRANYCH) {
-        std::unique_lock<std::mutex> lock(mtx);
-        cv.wait(lock, []{ return turaZakonczona; });
-
-        if (numerGracza == 1) {
-            wyborGracza1 = losujWybor();
-            std::cout << "Gracz 1 wybrał." << std::endl;
-        } else {
-            wyborGracza2 = losujWybor();
-            std::cout << "Gracz 2 wybrał." << std::endl;
-        }
-        turaZakonczona = false;
-        cv.notify_all();
-
-        // Oczekiwanie na wynik
-        cv.wait(lock, []{ return turaZakonczona; });
-    }
-}
-
 int main() {
-    std::thread gracz1(gracz, 1);
-    std::thread gracz2(gracz, 2);
+    int wygraneGracza1 = 0, wygraneGracza2 = 0;
+    int ostatniZwyciezca = 2; // Zakładamy, że gracz 2 zaczyna
 
     while (wygraneGracza1 < MAX_WYGRANYCH && wygraneGracza2 < MAX_WYGRANYCH) {
-        std::unique_lock<std::mutex> lock(mtx);
-        cv.wait(lock, []{ return !turaZakonczona; });
+        int wyborGracza1, wyborGracza2;
+
+        // Losowanie wyborów graczy w osobnych wątkach
+        std::thread t1([&]{ wyborGracza1 = losujWybor(1); });
+        std::thread t2([&]{ wyborGracza2 = losujWybor(2); });
+
+        t1.join();
+        t2.join();
 
         int wynik = decyduj(wyborGracza1, wyborGracza2);
-        if (wynik > 0) wygraneGracza1++;
-        else if (wynik < 0) wygraneGracza2++;
-
-        turaZakonczona = true;
-        cv.notify_all();
+        if (wynik == 1) {
+            std::cout << "Gracz 1 wygrywa rundę!" << std::endl;
+            wygraneGracza1++;
+            ostatniZwyciezca = 1;
+        } else if (wynik == -1) {
+            std::cout << "Gracz 2 wygrywa rundę!" << std::endl;
+            wygraneGracza2++;
+            ostatniZwyciezca = 2;
+        } else {
+            std::cout << "Remis!" << std::endl;
+            // Gracz, który przegrał ostatnią rundę, zaczyna
+            ostatniZwyciezca = ostatniZwyciezca == 1 ? 2 : 1;
+        }
     }
 
-    gracz1.join();
-    gracz2.join();
-
-    std::cout << "Koniec gry! Wyniki: Gracz 1 - " << wygraneGracza1 << ", Gracz 2 - " << wygraneGracza2 << std::endl;
+    std::cout << "Koniec gry! Wynik: Gracz 1 - " << wygraneGracza1 << ", Gracz 2 - " << wygraneGracza2 << std::endl;
     return 0;
 }
